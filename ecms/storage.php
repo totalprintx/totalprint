ce44702e28bf044b4f6d93b05e6102d41853981b
Marcel <marcelt91@live.de>
2015-01-16 15:01:07 +0100

    fffff

--- ecms/storage.php
+++ ecms/storage.php
@@ -1,3 +1,16 @@
+<h1>totalprint - STORAGE</h1>
 <?php
-	echo "horst";
+	$files = scandir("../tp_storage/");
+	foreach ($files as $key => $value) {
+		echo '<a href="../tp_storage/'.$value.'"><b>'.$value.'</b></a><br/>';
+	}
+?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <h2>Upload:</h2>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<?php if(isset($_GET['message'])) echo $_GET['message'];
 ?>
--- ecms/upload.php
+++ ecms/upload.php
@@ -1,14 +1,15 @@
 <?php
-$target_dir = "documents/";
+$target_dir = "../tp_storage/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 
 // if everything is ok, try to upload file
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        header('Location: storage.php');
     } else {
         echo "Sorry, there was an error uploading your file.";
+        header('Location: storage.php?message=Sorry, there was an error uploading your file.');
 }
 ?> 
