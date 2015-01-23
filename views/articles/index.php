1605cf19f4e413fe3b5f934a23552c4f1d6cfc7e
MHinzmann <hinzmann1993@yahoo.de>
2015-01-23 19:58:55 +0100

    Add pages for articles and other documents.

---
+++ views/articles/index.php
@@ -0,0 +1,22 @@
+<table class="table" border="1">
+	<tr>
+		<td>
+			Nr.
+		</td>
+		<td>
+			Datum
+		</td>
+		<td>
+			Titel
+		</td>
+		<td>
+			Verfasser
+		</td>
+		<td>
+			Veröffentlicht
+		</td>
+		<td>
+			Zuletzt bearbeitet
+		</td>
+	</tr>
+</table>
