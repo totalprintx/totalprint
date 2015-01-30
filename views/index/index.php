0941490a8c2ea8fbacdb516a470f95254f051a3f
MaikGroneberg <maik_grone@gmx.de>
2015-01-30 09:31:57 +0100

    search

--- views/index/index.php
+++ views/index/index.php
@@ -1,3 +1,9 @@
 <div class="jumbotron">
     <h2>Dashboard</h2>
+
+
+    <form action="ecms/search.php" method="post" enctype="multipart/form-data">
+    	<input type="search" name="searchbox" id="searchbox" value="Suchbegriff eingheben">
+   		<input type="submit" value="Suche" name="btn_search">
+</form>
 </div>
