5775ee89a31d3bbc45ecc3bac52fa2c905c1cb59
MINZMANN <mirco.hinzmann@gmail.com>
2015-02-05 13:03:19 +0100

    Add another PDO for the ecms-database.

--- libs/View.php
+++ libs/View.php
@@ -4,6 +4,7 @@ class View {
 	function __construct() {
 		$this->js = array();
 		$this->systems = $this->getSystemInfo();
+		$this->articles = $this->getArticles();
 	}
 
 	public function render($name, $noInclude = false)
@@ -18,13 +19,22 @@ class View {
 		}
 	}
 	public function getSystemInfo() {
-		$db = new Database();
-		$dataStatement = $db->prepare
-				('SELECT * From systems');        
+		$db = new DatabaseTP();
+		$dataStatement = $db->prepare('SELECT * From systems');        
 		$dataStatement->execute(array());
 
 		$data = $dataStatement->fetchAll();
 
+		return $data;
+	}
+	
+	public function getArticles() {
+		$db = new DatabaseEcms();
+		$dataStatement = $db->prepare('SELECT * FROM article');
+		$dataStatement->execute(array());
+		
+		$data = $dataStatement->fetchAll();
+		
 		return $data;
 	}
 
