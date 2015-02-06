46d90ab6aa9e68c8196b14049d865aaccd243b7f
MINZMANN <mirco.hinzmann@gmail.com>
2015-02-06 12:13:44 +0100

    Change database configuration.

--- config/database.php
+++ config/database.php
@@ -2,12 +2,12 @@
 
 define('DB_TP_TYPE', 'mysql');
 define('DB_TP_HOST', 'localhost');
-define('DB_TP_NAME', 'projekt');
-define('DB_TP_USER', 'root');
-define('DB_TP_PASS', '');
+define('DB_TP_NAME', 'lvps87-230-14-183.dedicated.hosteurope.de');
+define('DB_TP_USER', 'bas-erp');
+define('DB_TP_PASS', 'erplogin');
 
 define('DB_ECMS_TYPE', 'mysql');
-define('DB_ECMS_HOST', 'localhost');
-define('DB_ECMS_NAME', 'ecms');
-define('DB_ECMS_USER', 'root');
-define('DB_ECMS_PASS', '');
+define('DB_ECMS_HOST', 'lvps87-230-14-183.dedicated.hosteurope.de');
+define('DB_ECMS_NAME', 'ecm');
+define('DB_ECMS_USER', 'bas-erp');
+define('DB_ECMS_PASS', 'erplogin');
