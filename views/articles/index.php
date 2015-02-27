bb4922d9ee529c47fc40e2f10810c7c7eafbcd11
MINZMANN <mirco.hinzmann@gmail.com>
2015-02-27 18:14:45 +0100

    Move the x into the popup and format code.

--- views/articles/index.php
+++ views/articles/index.php
@@ -9,90 +9,86 @@
 <link rel="stylesheet" type="text/css" href="public/MarkItUp/markitup/sets/default/style.css" />
 
 <form action="articles/saveArticle" method="post" enctype="multipart/form-data">
-    <div id="popup">
-    
-        <div class="schliessen">X</div>
-        
-        <div id="popup_inhalt">
-            <h2 align="center">Artikel erstellen</h2>
-   			
-                <div class="row">
-                    <div class="col-sm-3 col-md-6 col-lg-4">Titel</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input name="titel" type="text" style="width:100%; margin-bottom: 2px;"></div>
-                    
-                    <div class="col-sm-3 col-md-6 col-lg-4">Verfasser</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input name="verfasser" type="text" style="width:100%; margin-bottom: 2px;"></div>
+	<div id="popup">
+		<div id="popup_inhalt">
+			<div class="schliessen" style="padding-top:4px">X</div>
+			
+				<h2 align="center">Artikel erstellen</h2>
+					<div class="row">
+						<div class="col-sm-3 col-md-6 col-lg-4">Titel</div>
+						<div class="col-sm-9 col-md-6 col-lg-8"><input name="titel" type="text" style="width:100%; margin-bottom: 2px;"></div>
+						
+						<div class="col-sm-3 col-md-6 col-lg-4">Verfasser</div>
+						<div class="col-sm-9 col-md-6 col-lg-8"><input name="verfasser" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Rubrik</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input name="rubrik" type="text" style="width:100%; margin-bottom: 2px;"></div>
+						<div class="col-sm-3 col-md-6 col-lg-4">Rubrik</div>
+						<div class="col-sm-9 col-md-6 col-lg-8"><input name="rubrik" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Ort des Geschehens</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input name="ort" type="text" style="width:100%; margin-bottom: 2px;"></div>
+						<div class="col-sm-3 col-md-6 col-lg-4">Ort des Geschehens</div>
+						<div class="col-sm-9 col-md-6 col-lg-8"><input name="ort" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Text</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><textarea id="markItUp" name="text"></textarea></div>
+						<div class="col-sm-3 col-md-6 col-lg-4">Text</div>
+						<div class="col-sm-9 col-md-6 col-lg-8"><textarea id="markItUp" name="text"></textarea></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Bilder</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8">
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile1" type="file" id="userfile1" style="margin-bottom: 2px;">
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile2" type="file" id="userfile2" style="margin-bottom: 2px;"> 
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile3" type="file" id="userfile3">
-                    </div>
-                </div>
-                <br>
-                <div  style="text-align:center;">
-                    <button type="submit" id="publish_article" name="action_type" value="publish_article">ver&ouml;ffentlichen</button>
-                    <button type="submit" id="save_article" name="action_type" value="save_article">speichern</button>
-                </div>
-        </div>
-    </div>
+						<div class="col-sm-3 col-md-6 col-lg-4">Bilder</div>
+						<div class="col-sm-9 col-md-6 col-lg-8">
+							<input type="hidden" name="max_file_size" value="1000000">
+							<input name="userfile1" type="file" id="userfile1" style="margin-bottom: 2px;">
+							<input type="hidden" name="max_file_size" value="1000000">
+							<input name="userfile2" type="file" id="userfile2" style="margin-bottom: 2px;"> 
+							<input type="hidden" name="max_file_size" value="1000000">
+							<input name="userfile3" type="file" id="userfile3">
+						</div>
+					</div>
+					</br>
+					<div style="text-align:center;">
+						<button type="submit" id="publish_article" name="action_type" value="publish_article">ver&ouml;ffentlichen</button>
+						<button type="submit" id="save_article" name="action_type" value="save_article">speichern</button>
+					</div>
+		</div>
+	</div>
 </form>
 
 
 <form action="articles/saveArticle" method="post" enctype="multipart/form-data">
-    <div id="popup_edit">
-    
-        <div class="schliessen_edit">X</div>
-        
-        <div id="popup_inhalt_edit">
-            <h2 align="center">Artikel bearbeiten</h2>
-   			
-                <div class="row">
-                    <div class="col-sm-3 col-md-6 col-lg-4">Titel</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input id="titel_edit" name="titel_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
-                    
-                    <div class="col-sm-3 col-md-6 col-lg-4">Verfasser</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input id="verfasser_edit" name="verfasser_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
+	<div id="popup_edit">
+		<div id="popup_inhalt_edit">
+			<div class="schliessen_edit" style="padding-top:4px">X</div>
+			<h2 align="center">Artikel bearbeiten</h2>
+		
+			<div class="row">
+				<div class="col-sm-3 col-md-6 col-lg-4">Titel</div>
+				<div class="col-sm-9 col-md-6 col-lg-8"><input id="titel_edit" name="titel_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
+				
+				<div class="col-sm-3 col-md-6 col-lg-4">Verfasser</div>
+				<div class="col-sm-9 col-md-6 col-lg-8"><input id="verfasser_edit" name="verfasser_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Rubrik</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input id="rubrik_edit" name="rubrik_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
+				<div class="col-sm-3 col-md-6 col-lg-4">Rubrik</div>
+				<div class="col-sm-9 col-md-6 col-lg-8"><input id="rubrik_edit" name="rubrik_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Ort des Geschehens</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><input id="ort_edit" name="ort_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
+				<div class="col-sm-3 col-md-6 col-lg-4">Ort des Geschehens</div>
+				<div class="col-sm-9 col-md-6 col-lg-8"><input id="ort_edit" name="ort_edit" type="text" style="width:100%; margin-bottom: 2px;"></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Text</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8"><textarea id="markItUp2" name="text"></textarea></div>
+				<div class="col-sm-3 col-md-6 col-lg-4">Text</div>
+				<div class="col-sm-9 col-md-6 col-lg-8"><textarea id="markItUp2" name="text"></textarea></div>
 
-                    <div class="col-sm-3 col-md-6 col-lg-4">Bilder</div>
-                    <div class="col-sm-9 col-md-6 col-lg-8">
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile1" type="file" id="userfile1" style="margin-bottom: 2px;">
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile2" type="file" id="userfile2" style="margin-bottom: 2px;"> 
-                        <input type="hidden" name="max_file_size" value="1000000">
-                        <input name="userfile3" type="file" id="userfile3">
-                    </div>
-                </div>
-                <br>
-                <div  style="text-align:center;">
-                    <button type="submit" id="publish_article" name="action_type" value="publish_article">ver&ouml;ffentlichen</button>
-                    <button type="submit" id="save_article" name="action_type" value="save_article">speichern</button>
-                </div>
-        </div>
-    </div>
+				<div class="col-sm-3 col-md-6 col-lg-4">Bilder</div>
+				<div class="col-sm-9 col-md-6 col-lg-8">
+					<input type="hidden" name="max_file_size" value="1000000">
+					<input name="userfile1" type="file" id="userfile1" style="margin-bottom: 2px;">
+					<input type="hidden" name="max_file_size" value="1000000">
+					<input name="userfile2" type="file" id="userfile2" style="margin-bottom: 2px;"> 
+					<input type="hidden" name="max_file_size" value="1000000">
+					<input name="userfile3" type="file" id="userfile3">
+				</div>
+			</div>
+			</br>
+			<div style="text-align:center;">
+				<button type="submit" id="publish_article" name="action_type" value="publish_article">ver&ouml;ffentlichen</button>
+				<button type="submit" id="save_article" name="action_type" value="save_article">speichern</button>
+			</div>
+		</div>
+	</div>
 </form>
 
 
